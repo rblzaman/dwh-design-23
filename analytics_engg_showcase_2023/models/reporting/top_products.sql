41a6290d59c6ce716ae0059669e6313cc2fcdbe0
rblzaman <rblzaman@gmail.com>
2023-10-20 09:02:28 +0200

    Creating Reporting Layer

---
+++ analytics_engg_showcase_2023/models/reporting/top_products.sql
@@ -0,0 +1,32 @@
+with store_device as (
+
+select d.id as device_id, 
+d.type, 
+d.store_id,
+s.name,
+s.city,
+s.country,
+s.typology
+from {{ ref ('stg_device')}} d
+left join {{ref ('stg_store')}}  s
+on d.store_id = s.id
+),
+
+store_device_trans as (
+  select sd.* , 
+    t.id as transaction_id,
+    t.product_name,
+    t.amount,
+    t.category_name,
+    t.status,
+    t.happened_at
+  from store_device sd
+  right join {{ ref ('stg_transaction')}} t
+  on sd.device_id = t.device_id
+)
+
+select product_name, sum(amount) as total_amount
+from store_device_trans
+group by product_name
+order by total_amount desc
+limit 10
